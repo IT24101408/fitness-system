3071935af68b06a42ad9271066f2cec0441a5b23
IT24101408 <it24101408@my.sliit.lk>
2025-05-09 00:00:07 +0530

    Spring boot Framework

---
+++ src/main/java/com/fitness_system/fitness_system/FitnessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fitness_system.fitness_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness_system/fitness_system/FitnessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness_system.fitness_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
